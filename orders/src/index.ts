67d0b99d2a8aa877112094a74001da2bf4eba8ba
sebslon <71407958+sebslon@users.noreply.github.com>
2022-10-12 14:35:14 +0200

    CICD: Test deployments (#5)

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -9,6 +9,8 @@ import { TicketUpdatedListener } from './events/listeners/ticket-updated-listene
 import { PaymentCreatedListener } from './events/listeners/payment-created-listener';
 
 const start = async () => {
+  console.log('Starting up...');
+
   if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined');
   if (!process.env.MONGO_URI) throw new Error('MONGO_URI must be defined');
   if (!process.env.NATS_URL) throw new Error('NATS_URL must be defined');
